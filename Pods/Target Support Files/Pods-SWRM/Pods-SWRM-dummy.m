91f6c2e57fae55e3b4067b9719ea2b6067b8a8d2
Ben Murphy <badesignco@icloud.com>
2017-12-23 11:33:21 -0800

    Add AudioKit Pod

---
+++ Pods/Target Support Files/Pods-SWRM/Pods-SWRM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SWRM : NSObject
+@end
+@implementation PodsDummy_Pods_SWRM
+@end
---
+++ Pods/Target Support Files/Pods-SWRMUITests/Pods-SWRMUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SWRMUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SWRMUITests
+@end
